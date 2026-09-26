ff33bbd0d236c3b861ea4b0545ced0c5500a73c0
Muhammad Ilyas <m.ilyasalfadlih@gmail.com>
2026-09-26 14:37:47 +0700

    Update Sabtu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,7 @@ $isStaffHost = auth()->user()->isStaffHostPubg()
     || auth()->user()->isStaffHostMonkeyPubg()
     || auth()->user()->isStaffHostFcMobile();
 $isStaffAdmin = auth()->user()->isStaffAdmin();
+$isAdminMenuRestricted = $isStaffAdmin || auth()->user()->isKoordinatorAdmin();
 $isStaffStock = auth()->user()->isStaffStock();
 $isStaffCreative = auth()->user()->isStaffCreative();
 $isCreativeMenuRestricted = $isStaffCreative || auth()->user()->isKoordinatorCreative();
@@ -139,7 +140,7 @@ if ($divisionViewUser) {
                 @endphp
 
                 <nav x-data="{ openMenu: @js($activeMenu) }"
-                     @click.capture="if ((@js($isStaffHost) && $event.target.closest('[data-development-menu] a')) || (@js($isStaffAdmin) && $event.target.closest('[data-development-admin-menu] a')) || (@js($isStaffStock) && $event.target.closest('[data-development-stock-menu] a')) || (@js($isCreativeMenuRestricted) && $event.target.closest('[data-development-creative-menu] a'))) { $event.preventDefault(); $store.toast.info('Menu divisi ini sedang dalam pengembangan.') }"
+                     @click.capture="if ((@js($isStaffHost) && $event.target.closest('[data-development-menu] a')) || (@js($isAdminMenuRestricted) && $event.target.closest('[data-development-admin-menu] a')) || (@js($isStaffStock) && $event.target.closest('[data-development-stock-menu] a')) || (@js($isCreativeMenuRestricted) && $event.target.closest('[data-development-creative-menu] a'))) { $event.preventDefault(); $store.toast.info('Menu divisi ini sedang dalam pengembangan.') }"
                      class="flex-1 overflow-y-auto p-4 space-y-1">
                     @if($isDivisionView)
                         @include('layouts.partials.division-sidebar', ['menu' => $activeDivisionMenu])
