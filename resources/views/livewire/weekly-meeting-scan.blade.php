f43b32b5043ca14eaa58cbe1e94fbd0a2fa4dfdd
unknown <m.ilyasalfadlih@gmail.com>
2026-09-26 09:14:21 +0700

    Update Sabtu

--- resources/views/livewire/weekly-meeting-scan.blade.php
+++ resources/views/livewire/weekly-meeting-scan.blade.php
@@ -130,16 +130,8 @@
                 </div>
 
                 <div id="scanner-container" class="relative">
-                    <div class="aspect-square lg:aspect-video bg-gray-900 rounded-xl overflow-hidden relative">
-                        <video id="scanner-video" class="w-full h-full object-cover" autoplay playsinline muted></video>
-                        <div class="absolute inset-0 flex items-center justify-center pointer-events-none">
-                            <div class="h-[90%] w-auto aspect-square border-2 border-primary-400 rounded-lg relative">
-                                <div class="absolute -top-2 -left-2 w-6 h-6 border-t-2 border-l-2 border-primary-400"></div>
-                                <div class="absolute -top-2 -right-2 w-6 h-6 border-t-2 border-r-2 border-primary-400"></div>
-                                <div class="absolute -bottom-2 -left-2 w-6 h-6 border-b-2 border-l-2 border-primary-400"></div>
-                                <div class="absolute -bottom-2 -right-2 w-6 h-6 border-b-2 border-r-2 border-primary-400"></div>
-                            </div>
-                        </div>
+                    <div class="aspect-video bg-gray-900 rounded-xl overflow-hidden relative">
+                        <video id="scanner-video" class="w-full h-full object-contain" autoplay playsinline muted></video>
                     </div>
                 </div>
 
